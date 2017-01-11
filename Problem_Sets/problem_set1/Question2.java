8f7847df8efcee80d80ec9adeef757814d78f587
Justin Ferrell <jferrell826@gmail.com>
2017-01-10 20:59:10 -0800

    Create Question2.java

---
+++ Problem_Sets/problem_set1/Question2.java
@@ -0,0 +1,19 @@
+// Write a program that will print the word Udacity in a box
+// so that it looks exactly like the figure below. Name the program NameInBox
+
+// +-------+
+// |Udacity|
+// +-------+ 
+//
+        
+
+public class NameInBox
+{
+	public static void main(String[] args)
+	{
+	    System.out.println("+-------+");
+        System.out.println("|Udacity|");
+        System.out.println("+-------+");
+        //TODO finish the draft so that your code prints Udacity in a box
+	}
+}
