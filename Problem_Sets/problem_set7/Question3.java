9e81b50fb409a020a8de3c7aee5af3c7f6c30386
Justin Ferrell <jferrell826@gmail.com>
2017-01-10 23:15:25 -0800

    Create Question3.java

---
+++ Problem_Sets/problem_set7/Question3.java
@@ -0,0 +1,47 @@
+//This problem will use the same data as the previous one. This time you are
+//to complete the TemperatureDifferenceCalculator. It has two methods:
+
+//public double maxDifference() - Calculates the maximum difference between
+//any two consecutive days.
+
+//public double minDifference() - Calculates the minimum difference between
+//any two consecutive days
+
+//If Jan 7 temperature is 5 degrees and Jan 8 is -10 degrees, the difference
+//between the two temperatures is 15. Not that a temperature drop is simply
+//considered a 15 degree change. The difference is always the absolute value.
+//Assume that the days are in order in the array, so data[0] is the first day,
+//data[1] is the second day, and so on.
+
+//Need help starting this question? In the lesson titled 
+//"Starting points: Problem Set Questions", go to the
+//problem titled "Problem Set 7 - Question 3" for some tips on 
+//how to begin.
+
+public class TemperatureDifferenceCalculator
+{
+    private double[] data;
+
+   /**
+    * Constructs a TemperatureDifferenceCalculator with the given array
+    * @param the array to process
+    */
+    public TemperatureDifferenceCalculator(double[] list)
+    {
+        data = list;
+    }
+
+   /**
+    * Gets the maximum difference between any two consecutive values
+    * @return the maximum difference
+    */
+
+    // TODO: create maxDifference method.  Remeber to give it a return statement.
+
+   /**
+    * Gets the minimum difference between any two consecutive values
+    * @return the minimum difference
+    */
+
+    // TODO: create minDifference method. Remember to give it a return statement.
+}
