f9cab3eeea368b97933889a8680a67d61799326e
Justin Ferrell <jferrell826@gmail.com>
2017-01-10 20:58:42 -0800

    Create Question1.java

---
+++ Problem_Sets/problem_set1/Question1.java
@@ -0,0 +1,17 @@
+//What does this program print?
+
+public class Test 
+{
+  public static void main(String [] args) 
+  {
+    System.out.println("42 + 7");
+    System.out.println(42 + 7);
+    System.out.print("Hello");
+    System.out.println("World");
+   }
+ }
+ 
+ //ANSWER 
+ //42 + 7
+ //49
+ //HelloWorld
